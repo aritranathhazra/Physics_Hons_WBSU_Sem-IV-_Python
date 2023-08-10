1927dd5a9f6f37494dc59c256b6230532d22a4a3
Aritra Nath Hazra <100029610+aritranathhazra@users.noreply.github.com>
2023-08-10 18:29:11 +0530

    Add files via upload

---
+++ boundary_heatflow.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+c=np.linspace(0,1,101)
+u=np.exp(-(c)**2/0.001) # continuous heat at end point x=0
+
+for time in range(1000):
+    u[1:-1] += 0.25*(u[2:]-2*u[1:-1]+u[:-2])
+        
+plt.plot(c,u,'.-',label='for time = 1000')
+plt.plot(c,np.exp(-(c)**2/0.001), '--', label = 'for time = 0')
+plt.title("Profile of heat flow in 1-D for two different times")
+plt.xlabel("Position")
+plt.ylabel("Intensity of heat energy")
+plt.legend()
+plt.show()
+
---
+++ boundary_wave.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n=int(input(" enter the time step needed "))
+
+x=np.zeros((181))
+u=np.zeros((181,n+2))
+
+#initial condition:-(here it is '1st excited state')
+x[:]=np.array([i*np.pi/180 for i in range(181)])
+u[:,0]=np.sin(x)
+
+for j in range(0,n+1):
+    for i in range(1,180):
+
+        if j==0:
+            u[i,j+1]=u[i+1,j]+u[i-1,j]-(u[i+1,j]+u[i-1,j])/2
+        else:
+            u[i,j+1]= (u[i+1,j]+u[i-1,j]-u[i,j-1])
+
+#for j in range(0,n+1):
+#    if j==0:
+#        u[1:-1,j+1]=u[:-2,j+1]+u[2:,j]-(u[:-2,j]+u[2:,j])/2
+#    else:
+#        u[1:-1,j+1]= (u[:-2,j]+u[2:,j]-u[1:-1,j-1])
+
+s=np.zeros(181)        
+
+s[:]=u[:,n-2]
+
+plt.plot(np.sin(x),'.-',label='Initial configuration')
+plt.plot(s,'--',label='after timestep = 70')
+plt.title("Profile of wave on a string fixed at two ends")
+plt.xlabel("Position on string")
+plt.ylabel("Amplitude of wave on a string")
+plt.ylim(0.0,1.5)
+plt.legend()
+plt.show()
+
+#-------------------input----------------------
+#enter the time step needed 70
+#----------------------------------------------
