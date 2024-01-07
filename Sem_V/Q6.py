55e4ba2b9777114ac70696333637b2529c4c92cb
Deep Kr. Ghosh <75083264+deepkrg17@users.noreply.github.com>
2024-01-07 20:40:05 +0530

    Added Q6 code

---
+++ Sem_V/Q6.py
@@ -0,0 +1,64 @@
+#  Q6  probability for position of electron in H atom
+import math as mt
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.integrate as intg
+import scipy.special as sc
+
+
+def sprcl(l,m,theta):
+    Y_lm = (-1)**m *np.sqrt((2*l+1.)*mt.factorial(l-m)/(4.*np.pi*mt.factorial(l+m)))
+    Y_lm = Y_lm * sc.lpmn(m,l,np.cos(theta))[0][m,l]
+    return Y_lm
+
+def radial(n,l,x):
+    R_nl = mt.factorial(n+l)*mt.exp(-x/n) *(2*x/n)**l *sc.assoc_laguerre(2*x/n,n-l-1,2*l+1)
+    R_nl = R_nl * np.sqrt((2/n)**3 *mt.factorial(n-l-1)/((2*n)*mt.factorial(n+l)**3))
+    return R_nl
+
+points = 1000
+div1 = np.pi/(points-1)
+angle = np.arange(0, np.pi, div1)
+div2 = 1/(points-1)
+rad = np.arange(0, 100, div2)
+pol = np.arange(0, 2*np.pi, 2*div1)
+
+for i in range(1,3):        #i-->n
+    for j in range(0,i):        #j-->l
+        for k in range(-j,j+1):     #k-->m
+            for a in range(1,3):        #a-->n_prime
+                for b in range(0,a):        #b-->l_prime
+                    for c in range(-b,b+1):     #c-->m_prime
+                        
+                        s_h = [sprcl(j,k,th)*sprcl(b,c,th)*np.sin(th) for th in angle]
+                        r = [radial(i,j,radius)*radial(a,b,radius)*radius**2 for radius in rad]
+                        az1 = intg.quad(lambda phi: np.sin((k-c)*phi), 0, 2*np.pi)[0]
+                        az2 = intg.quad(lambda phi: np.cos((k-c)*phi), 0, 2*np.pi)[0]
+                        az = np.sqrt(az1**2 +az2**2)
+                        integration = intg.simps(s_h,angle)*intg.simps(r, rad)*az
+                        if (i==1 and a==1 and j==0 and b==0 and k==0 and c==0):
+                             plt.polar(pol, s_h,"g.")
+                             plt.title("Angular probability density distribution for ground state")
+                             plt.show()
+                             plt.plot(rad, r,)
+                             plt.title("Radial pobability density distribution for ground state")
+                             plt.xlim(0,20)
+                             plt.xlabel("$r$")
+                             plt.ylabel("$|rR_{00}(r)|^{2}$")
+                             plt.show()
+                        elif (i==2 and a==2 and j==0 and b==0 and k==0 and c==0):
+                             plt.polar(pol, s_h,"g.")
+                             plt.title("Angular probability density distribution for 1st excited state")
+                             plt.show()
+                             plt.plot(rad, r,)
+                             plt.title("Radial pobability density distribution for 1st excited state")
+                             plt.xlim(0,20)
+                             plt.xlabel("$r$")
+                             plt.ylabel("$|rR_{00}(r)|^{2}$")
+                             plt.show()
+                        print("n=",i,"n_prime=",a,"l=",j,"l_prime=",b,"m=",k,"m_prime=",c,":--")
+                        print("----------------------------------------------")
+                        print("Value of integration = ",integration)
+                        print("\n")
+
